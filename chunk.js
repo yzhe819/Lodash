94922cb1c897ef4ef9e863ef22b3b3c7b9f82158
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-04-01 10:53:47 +1300

    add: array compact

--- chunk.js
+++ chunk.js
@@ -1,18 +1,5 @@
-/**
- * Creates an array of elements split into groups the length of `size`.
- * If `array` can't be split evenly, the final chunk will be the remaining
- * elements.
- *
- * Math ceil => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/ceil
- * array slice => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
- *
- * @category Array
- * @param {Array} array The array to process.
- * @param {number} [size=1] The length of each chunk
- * @returns {Array} Returns the new array of chunks.
- * @example
- *
- */
+// Creates an array of elements split into groups the length of size.
+// If array can't be split evenly, the final chunk will be the remaining elements.
 const chunk = (array, size = 1) => {
     size = Math.max(1, size);
     return [...Array(Math.ceil(array.length / size))].map((_, i) => array.slice(i * size, i * size + size));
