d3a455b1762ac30a418d4968ebd2a7c016d87c13
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-04-04 15:44:00 +1200

    add: array findLastIndex

--- __tests__/findLastIndex.test.js
+++ __tests__/findLastIndex.test.js
@@ -1,10 +1,10 @@
-import findIndex from "../findIndex";
-it("should return index of users whose ago is greater than or equal to 40 ", () => {
+import findLastIndex from "../findLastIndex";
+it("should return last index of users whose ago is greater than or equal to 40 ", () => {
   const users = [
     { user: "barney", age: 36, active: true },
     { user: "fred", age: 40, active: false },
     { user: "pebbles", age: 1, active: true },
     { user: "patricia", age: 42, active: true },
   ];
-  expect(findIndex(users, (o) => o.age >= 40)).toStrictEqual(1);
+  expect(findLastIndex(users, (o) => o.age >= 40)).toStrictEqual(3);
 });
---
+++ findLastIndex.js
@@ -0,0 +1,7 @@
+// Return the index of the last element that predicate returns truthy instead of the element itself.
+const findLastIndex = (array, predicate) => {
+  const reverseIndex = array.reverse().findIndex(predicate);
+  return reverseIndex === -1 ? -1 : array.length - (reverseIndex + 1);
+};
+
+module.exports = findLastIndex;
