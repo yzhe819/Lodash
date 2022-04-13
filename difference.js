be34221da15f207288b1cdf5fbe8f86fa15d95b6
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-04-13 17:05:18 +1200

    ref: difference and differenceBy

--- difference.js
+++ difference.js
@@ -1,13 +1,8 @@
 // Creates an array of array values not included in the other given arrays.
 // The order and references of result values are determined by the first array.
 const difference = (array, ...values) => {
-    if (Array.isArray(array)) {
-        const validValues = values.filter((value) => Array.isArray(value));
-        const combinedValues = [].concat(...validValues);
-        array = array.map((item) => (item == 0 ? 0 : item));
-        return array.filter((item) => !combinedValues.includes(item));
-    } else {
-        return [];
-    }
+    if (!Array.isArray(array)) return [];
+    const validValues = [].concat(...values.filter((value) => Array.isArray(value)));
+    return array.map(i => i == 0 ? 0 : i).filter((item) => !validValues.includes(item));
 };
 module.exports = difference;
--- differenceBy.js
+++ differenceBy.js
@@ -2,17 +2,13 @@
 // which is invoked for each element of array and values to generate the criterion by which they're compared.
 const differenceBy = (array, ...values) => {
     if (!Array.isArray(array)) return [];
+    const validValues = [].concat(...values.filter((value) => Array.isArray(value)));
     let iteratee = values.pop();
-    const validValues = values.filter((value) => Array.isArray(value));
-    let combinedValues = [].concat(...validValues);
-    array = array.map((item) => (item == 0 ? 0 : item));
-    if (typeof iteratee === "function") {
-        combinedValues = combinedValues.map((value) => iteratee(value));
-        return array.filter(
-            (item) => !combinedValues.map(iteratee).includes(iteratee(item)));
-    } else {
-        return array.filter(
-            (item) => !combinedValues.map((value) => value[iteratee]).includes(item[iteratee]));
+    if (typeof iteratee == 'string') {
+        const prop = iteratee;
+        iteratee = ((item) => item[prop]);
     }
+    return array.map(i => i == 0 ? 0 : i).filter(
+        (item) => !validValues.map(iteratee).includes(iteratee(item)));
 };
 module.exports = differenceBy;
