98e7850712d7e0584e10e514d64602eb94f3c70a
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-04-13 16:37:36 +1200

    add: array intersectionBy

---
+++ intersectionBy.js
@@ -0,0 +1,13 @@
+// returns the new array of intersecting values.
+const intersectionBy = (array, ...values) => {
+    let iteratee = values.pop();
+    if (typeof iteratee === 'string') {
+        const prop = iteratee;
+        iteratee = ((item) => item[prop]);
+    }
+    return array.filter(a =>
+        values.every(list => list.find(b => iteratee(b) === iteratee(a)))
+    );
+}
+
+module.exports = intersectionBy;
