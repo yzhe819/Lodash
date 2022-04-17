b1b42215e79c062dacc87ec5b11950b531e61357
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-04-18 00:11:11 +1200

    fix: array pull

--- __tests__/pull.test.js
+++ __tests__/pull.test.js
@@ -1,7 +1,18 @@
 import pull from '../pull';
 it('should modify and return the array', () => {
-    let actual = pull([1, 2, 3], [1, 3]);
+    let array = [1, 2, 3];
+    let actual = pull(array, [1, 3]);
     expect(actual).toStrictEqual([2]);
+    expect(array).toStrictEqual([2]);
+});
+it('should preserve holes in arrays', () => {
+    let array = [1, 2, 3, 4];
+    delete array[1];
+    delete array[3];
+
+    let actual = pull(array, [1]);
+    expect(!('0' in array)).toBe(true);
+    expect(!('2' in array)).toBe(true);
 });
 it('should treat holes as `undefined`', () => {
     let array = [1, 2, 3];
--- pull.js
+++ pull.js
@@ -1,8 +1,11 @@
 // Removes all given values from array
 const pull = (array, ...values) => {
     values = values.flat(Infinity);
-    // use concat and ... to handle holes in array
-    return [].concat(...array).filter(value => !values.includes(value));
+    for (let i = 0; i < array.length; i++) {
+        values.includes(array[i]) && array.splice(i--, 1);
+    }
+    // use ... to handle holes in array
+    return [...array];
 };
 
 module.exports = pull;
